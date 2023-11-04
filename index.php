621f5fa3c75d3a74d62fffa5c0a8dcbbe9d95608
Mati18505 <mstulczewski@gmail.com>
2023-11-04 11:03:08 +0100

    Added html metadata.

--- index.php
+++ index.php
@@ -22,6 +22,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <meta name="descritpion" content="A Nostale private game server with many addons.">
+    <meta name="keywords" content="Nostale, Server">
+    <meta name="author" content="Mati18505">
 </head>
 
 <body>
